1ed1b96eb5537b907afeed627eeb38056dae6bbb
noface <noface@outlook.com>
2017-05-07 07:06:11 +0200

    do not insert css multiple times

--- pack.sh
+++ pack.sh
@@ -2,6 +2,6 @@
 
 webpack -p --optimize-minimize
 mkdir -p build
-cat userscript.txt > build/bundle.user.js
+cat userscript.txt > build/dragonchan.user.js
 cat bundle.js >> build/dragonchan.user.js
 rm bundle.js
--- src/BossDisplay.js
+++ src/BossDisplay.js
@@ -1,5 +1,9 @@
 const appendStyle = () => {
+  if (document.getElementById('dragonchan-css')) {
+    return;
+  }
   const css = document.createElement("style");
+  css.id = 'dragonchan-css';
   css.type = "text/css";
   css.innerHTML = '.hp-bar{position:relative;width:500px;font-family:Tahoma;font-size:22px;margin-bottom:30px}.hp-bar .health-remaining-text,.hp-bar .health-total-text{letter-spacing:.1em;font-family:Visitor}.hp-bar .health{float:left;border:1px solid #999;border-radius:4px;width:100%}.hp-bar .health-remaining{float:left;background:#247f00;height:20px}.hp-bar .health-remaining-text{text-align:center;margin-top:10px}.hp-bar .health-total-text{position:absolute;top:25px;right:3px}.hp-bar .health-total{float:left;height:20px}.hp-bar .red{background:#af0000}.hp-bar .orange{background:#ea7900}.hp-bar .yellow{background:#eacc00}';
   document.body.appendChild(css);
