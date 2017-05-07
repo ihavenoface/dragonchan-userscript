a93831ad8fff81cd2a1c8c642dff4d4dc1ecd063
noface <noface@outlook.com>
2017-05-07 07:43:45 +0200

    Revert "crossorigin.me to force CORS for now"
    This reverts commit 2847043a38039c5c62cdb7d1a3c2eaa90e0ceb55.

--- src/fetchBackend.js
+++ src/fetchBackend.js
@@ -7,7 +7,7 @@ const fetchBackend = () => {
   g.posts = [...document.querySelectorAll('.thread > .postContainer')];
   g.OP = g.posts.splice(0, 1)[0];
 
-  fetch(`https://crossorigin.me/http://drgchan.ddns.net/${g.OP.id.slice(2)}`)
+  fetch(`http://drgchan.ddns.net/${g.OP.id.slice(2)}`)
     .then(res => res.text())
     .then(res => {
       const parser = new DOMParser();
