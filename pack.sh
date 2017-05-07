1e4cd05293b9439338df8d1edda6a111a4533c74
noface <noface@outlook.com>
2017-05-07 06:46:06 +0200

    test travis

--- pack.sh
+++ pack.sh
@@ -3,5 +3,5 @@
 webpack -p --optimize-minimize
 mkdir -p build
 cat userscript.txt > build/bundle.user.js
-cat bundle.js >> build/bundle.user.js
+cat bundle.js >> build/dragonchan.user.js
 rm bundle.js
