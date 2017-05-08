69c6b33d58063557fc5efa8b0015c845c4640d68
noface <noface@outlook.com>
2017-05-08 16:45:49 +0200

    try scrolling back to previous position after fetching and inserting data

--- src/BossDisplay.js
+++ src/BossDisplay.js
@@ -5,8 +5,11 @@ const BossDisplay = (doc) => {
   if (!health) {
     return;
   }
+  const sidebar = doc.querySelector('.sidebar');
+  let scrollPosition = 0;
   const previousNode = document.body.querySelector('.ink-l60');
   if (previousNode) {
+    scrollPosition = previousNode.querySelector('.sidebar').scrollTop;
     previousNode.parentNode.removeChild(previousNode);
   }
   const healthBar = health.querySelector('.health');
@@ -19,11 +22,11 @@ const BossDisplay = (doc) => {
   style.position = 'fixed';
   style.top = '40px';
   style.right = '10px';
-  const sidebar = doc.querySelector('.sidebar');
   sidebar.style.overflowY = 'scroll';
   sidebar.style.height = '330px';
   health.appendChild(sidebar);
   document.body.appendChild(health);
+  sidebar.scrollTop = scrollPosition;
 };
 
 export default BossDisplay;
