32f05a1431b5224cc43e7a25591272c37836853c
noface <noface@outlook.com>
2017-05-19 00:30:05 +0200

    clean up boss stats a bit

--- src/BossDisplay.js
+++ src/BossDisplay.js
@@ -1,32 +1,35 @@
 import fetchBackend from './fetchBackend';
 import c from './constants/BossDisplay';
 
+let container = null;
+
+const initContainer = () => {
+  if (container) {
+    return;
+  }
+  container = document.createElement('div');
+  container.className = c.CONTAINER;
+  document.body.appendChild(container);
+};
+
 const BossDisplay = (doc) => {
+  initContainer();
   const health = doc.querySelector(c.HEALTH_SELECTOR);
   if (!health) {
     return;
   }
   const sidebar = doc.querySelector(c.SIDEBAR_SELECTOR);
+  const healthBar = health.querySelector(c.HEALTHBAR_SELECTOR);
+  healthBar.addEventListener('click', fetchBackend);
+  healthBar.title = 'Refresh stats';
+  health.appendChild(sidebar);
+  const previousNode = container.querySelector(c.HEALTH_SELECTOR);
   let scrollPosition = 0;
-  const previousNode = document.body.querySelector(c.HEALTH_SELECTOR);
   if (previousNode) {
     scrollPosition = previousNode.querySelector(c.SIDEBAR_SELECTOR).scrollTop;
     previousNode.parentNode.removeChild(previousNode);
   }
-  const healthBar = health.querySelector(c.HEALTHBAR_SELECTOR);
-  healthBar.addEventListener('click', () => {
-    fetchBackend();
-  });
-  healthBar.style.cursor = 'pointer';
-  healthBar.title = 'Refresh stats';
-  const { style } = health;
-  style.position = 'fixed';
-  style.top = '40px';
-  style.right = '10px';
-  sidebar.style.overflowY = 'scroll';
-  sidebar.style.height = '330px';
-  health.appendChild(sidebar);
-  document.body.appendChild(health);
+  container.appendChild(health);
   sidebar.scrollTop = scrollPosition;
 };
 
--- src/constants/BossDisplay.js
+++ src/constants/BossDisplay.js
@@ -1,4 +1,6 @@
 export default {
+  CONTAINER: 'dragonchan boss-stats',
+  CONTAINER_SELECTOR: '.dragonchan.boss-stats',
   HEALTH_SELECTOR: '.ink-l60',
   HEALTHBAR_SELECTOR: '.health',
   SIDEBAR_SELECTOR: '.sidebar',
